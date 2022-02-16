0118e8d3953851f9c73d4d0a8f7e8c156b027283
6050 <48523652+6050@users.noreply.github.com>
2022-02-17 01:30:21 +0900

    Create 13985.py

---
+++ 2022/01/13985.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+
+        eq = list(input().split())
+        if int(eq[0]) + int(eq[2]) == int(eq[4]):
+                print("YES")
+        else:
+                print("NO")
