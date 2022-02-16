8bcc320fab6a21bc449cc0c0f34e6bcf1fe4336a
6050 <48523652+6050@users.noreply.github.com>
2022-02-17 01:07:46 +0900

    Update 1259.py

--- 2021/11/1259.py
+++ 2021/11/1259.py
@@ -2,31 +2,9 @@ if __name__ == '__main__':
 
     str = input()
     while str != "0":
-        if len(str) == 1:
+        if str == str[::-1]:
             print("yes")
-
-        elif len(str) == 2:
-            if str[0] == str[1]:
-                print("yes")
-            else:
-                print("no")
-
-        elif len(str) == 3:
-            if str[0] == str[2]:
-                print("yes")
-            else:
-                print("no")
-
-        elif len(str) == 4:
-            if str[0] == str[3] and str[1] == str[2]:
-                print("yes")
-            else:
-                print("no")
-
-        elif len(str) == 5:
-            if str[0] == str[4] and str[1] == str[3]:
-                print("yes")
-            else:
-                print("no")
+        else:
+            print("no")
 
         str = input()
