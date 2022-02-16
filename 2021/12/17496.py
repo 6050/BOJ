0b30444a2f14ab6c5e432dd3679de28345dea96f
6050 <48523652+6050@users.noreply.github.com>
2022-02-17 01:24:41 +0900

    Create 17496.py

---
+++ 2021/12/17496.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+
+        N, T, C, P = map(int, input().split())
+        if N % T == 0:
+                print((N // T - 1) * C * P)
+        else:
+                print(N // T * C * P)
