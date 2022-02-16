2498e8f1ff85271157f8835dc5b8ff38f295c044
6050 <48523652+6050@users.noreply.github.com>
2022-02-17 01:28:56 +0900

    Create 2480.py

---
+++ 2022/01/2480.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+
+        A, B, C = map(int, input().split())
+
+        if A == B and B == C:
+                print(10000 + A * 1000)
+        elif A == B and B != C:
+                print(1000 + A * 100)
+        elif B == C and A != B:
+                print(1000 + B * 100)
+        elif A == C and A!= B:
+                print(1000 + A * 100)
+        elif A != B and B != C:
+                print(max(A, B, C) * 100)
