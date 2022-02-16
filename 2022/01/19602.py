f876bd1ece289c3611d1882109c52905c15757d9
6050 <48523652+6050@users.noreply.github.com>
2022-02-17 01:34:16 +0900

    Create 19602.py

---
+++ 2022/01/19602.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+
+    S = int(input())
+    M = int(input())
+    L = int(input())
+
+    if S + 2 * M + 3 * L >= 10:
+        print("happy")
+    else:
+        print("sad")
